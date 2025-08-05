951e3d4415e5b774f154c6de03c6cf727b00f1e1
hasikarao <2420030227@klh.edu.in>
2025-08-05 11:25:05 +0530

    Add files via upload

---
+++ auto_cast.java
@@ -0,0 +1,11 @@
+package Myproject;
+
+public class auto_cast {
+	public static void main(String[] args) {
+		int myint=9;
+		double mydouble =myint;
+		System.out.println(myint);
+		System.out.println(mydouble);
+	}
+
+}
